c5c3ef2d4a9fc84137be9c1b68c64e7da808d381
Venkat Koushik <144405842+vkoushik15@users.noreply.github.com>
2025-03-18 21:50:58 +0530

    Update conn.js

--- server/conn/conn.js
+++ server/conn/conn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const Db = async () => {
     try {
-        await mongoose.connect("mongodb+srv://venkatkoushik15:Mn7UlWFxTtM64b04@rarity.27iht.mongodb.net/myDatabase?retryWrites=true&w=majority")
+        await mongoose.connect(PROCESS.ENV.URI)
         console.log('vooonected to themongdb')
     } catch (error) {
         console.log('error in connecting to db',error)
