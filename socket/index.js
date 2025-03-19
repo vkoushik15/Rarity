12847d3f35d9b85fbdd59c19aa0ef40a04fe7c28
Venkat Koushik <144405842+vkoushik15@users.noreply.github.com>
2025-03-19 12:21:28 +0530

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -1,71 +1,4 @@
 
-// const express = require("express");
-// const http = require("http");
-// const socketIo = require("socket.io");
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIo(server, {
-//     cors: {
-//         origin: "http://localhost:5173", 
-//         methods: ["GET", "POST"],
-//     },
-// });
-
-
-// let activeUsers = [];
-
-// io.on("connection", (socket) => {
-//     console.log("New client connected: ", socket.id);
-
-    
-
-//     socket.on("new-user-add", (userId) => {
-        
-//         console.log(userId)
-        
-//         if (!activeUsers.some((user) => user.userId === userId)) {
-            
-//             activeUsers.push({ userId, socketId: socket.id });
-//             console.log(`User ${userId} connected`);
-//         }
-//         io.emit("active-users", activeUsers); 
-//        console.log(activeUsers)
-//     });
-
-    
-//     socket.on("send-message", (messageData) => {
-//         const { receiverId } = messageData;
-//         const receiver = activeUsers.find((user) => user.userId ===receiverId );
-       
-//         if (receiver) {
-//             console.log('message from socketis',messageData)
-//             console.log('recsocid',receiver.socketId)
-
-//             io.to(receiver.socketId).emit("receive-message", messageData)
-    
-//              // Send message to receiver
-//         }
-//     });
-//     socket.on("new-bid",(bidData)=>{
-//         console.log('new bid recived :',bidData)
-//         io.emit('update-bids',bidData)
-//     })
-
-//     // When a user disconnects
-//     socket.on("disconnect", () => {
-//         activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
-//         console.log(`User disconnected: ${socket.id}`);
-//         io.emit("active-users", activeUsers); // Update active users list
-//     });
-// });
-
-// // Start the server
-// server.listen(8800, () => {
-//     console.log("Server running on http://localhost:8800");
-// });
-
-
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
@@ -105,10 +38,10 @@ io.on("connection", (socket) => {
     // Notify all clients about a new chat
     socket.on("new-chat", (chatData) => {
         console.log("New chat created:", chatData);
-        io.emit("update-chats", chatData); // Notify all clients
+        io.emit("update-chats", chatData); 
     });
 
-    // Handle new bids
+  
     socket.on("new-bid", (bidData) => {
         console.log("New bid received:", bidData);
         io.emit("update-bids", bidData);
